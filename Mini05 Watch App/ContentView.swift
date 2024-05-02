5c8f2905d8fe09f72a427c4ee0b1a5e28f1a32c0
Felipspp <felipe-prates9@hotmail.com>
2024-05-02 15:12:09 -0300

    Initial Commit

---
+++ Mini05 Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Mini05 Watch App
+//
+//  Created by Felipe Porto on 02/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Mini05 Watch App/Mini05App.swift
@@ -0,0 +1,17 @@
+//
+//  Mini05App.swift
+//  Mini05 Watch App
+//
+//  Created by Felipe Porto on 02/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Mini05_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
